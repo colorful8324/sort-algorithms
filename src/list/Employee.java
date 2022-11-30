7df176a9ffeadb41becaa647351fcff966892850
colorful <khanh501050105@gmail.com>
2022-12-01 02:12:14 +0700

    Added list

---
+++ src/list/Employee.java
@@ -0,0 +1,65 @@
+package list;
+
+import java.util.Objects ;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private int id;
+
+
+    public Employee(String firstName, String lastName, int id) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Employee)) {
+            return false;
+        }
+        Employee employee = (Employee) o;
+        return Objects.equals(firstName, employee.firstName) && Objects.equals(lastName, employee.lastName) && id == employee.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, id);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " firstName='" + getFirstName() + "'" +
+            ", lastName='" + getLastName() + "'" +
+            ", id='" + getId() + "'" +
+            "}";
+    }
+}
